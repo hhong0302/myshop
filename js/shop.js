ca8ac3d4716266cd8881d84c42a5f0e0088a2c06
hong <124054037+hhong0302@users.noreply.github.com>
2023-03-14 17:49:43 +0900

    first commit

---
+++ js/shop.js
@@ -0,0 +1,155 @@
+(function()
+{
+    const listAll = document.getElementsByClassName('list-all')[0];
+    const cart = document.getElementById('cart');
+    const close = document.getElementById('close');
+
+        listAll.addEventListener('click',changeNav);
+        cart.addEventListener('click',cartBoxView);
+        close.addEventListener('click',cartBoxView);
+        
+        
+
+    function changeNav()
+    {
+        const nav = document.getElementsByTagName('nav')[0].offsetTop + 52;
+        const sitemap = document.getElementById('sitemap')
+        listAll.classList.toggle('close');
+        listAll.classList.toggle('newlist');
+        sitemap.classList.toggle('vmode');
+        if(listAll.className == 'list-all close')
+        {
+            sitemap.style.top = nav+ 'px';
+            sitemap.style.display='block';
+        }
+        else
+        {
+            sitemap.style.display='none';
+        }
+    }
+
+    function cartBoxView()
+    {
+        document.getElementsByClassName('cart-view')[0].classList.toggle('none');
+    }
+
+    //슬라이드 쇼
+
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides()
+    {
+        let i ;
+        const slides = document.getElementsByClassName('img-slide');
+        const dot = document.getElementsByClassName('dot');
+        
+        for (i = 0 ; i < slides.length ; i++)
+        {
+            slides[i].style.display = 'none';
+            dot[i].classList.remove('active');
+        }
+
+        slideIndex++;
+
+        if(slideIndex > slides.length)
+        {
+            slideIndex = 1;
+        }
+        
+        slides[slideIndex - 1].style.display = 'block';
+        dot[slideIndex -1].classList.add('active');
+
+        setTimeout(showSlides, 5000);
+    }
+
+    
+}());
+
+
+//bestlink
+
+document.getElementsByClassName('tablinks')[0].click();
+
+function openBest(e , bid)
+{
+    const tabcontent = document.getElementsByClassName('best-tabcontent');
+    for(let i = 0 ; i < tabcontent.length ; i++)
+    {
+        tabcontent[i].style.display = 'none';
+    }
+
+    const tablinks = document.getElementsByClassName("tablinks");
+    for(let i = 0 ; i < tablinks.length ; i++)
+    {
+        tablinks[i].classList.remove('active');
+    }
+
+    document.getElementById(bid).style.display = "block";
+    e.currentTarget.classList.add('active');
+
+}
+
+
+function view(e)
+{
+    const tabcontent = document.getElementsByClassName('tabcontent');
+    const tabs = document.getElementsByClassName('tab')[0];
+
+    // const li = document.getElementsByClassName('tabli');
+
+    for(let i = 0 ; i<tabcontent.length ; i ++)
+    {
+        tabcontent[i].classList.remove('active');
+        tabs.children[i].classList.remove('active');
+
+        // li[i].classList.remove('active');
+    }
+
+    tabcontent[e].classList.add('active');
+    tabs.children[e].classList.add('active');
+
+    // li[e].classList.add('active');
+}
+
+
+/* function product(num,col)
+{
+    const colAr = ['col-lg-12','col-lg-4' , 'col-lg-3'];
+    const products = document.getElementsByClassName('product-images');
+    const libox = document.getElementsByClassName('libox');
+
+    for(let i = 0 ; i<products.length ; i++)
+    {
+        products[i].classList.remove(colAr[0],colAr[1],colAr[2]);
+    }
+
+    for(let i = 0 ; i<libox.length ; i++)
+    {
+        libox[i].classList.remove('active');
+    }
+
+    for(let i = 0 ; i<products.length ; i++)
+    {
+        products[i].classList.add(col);
+    }
+
+    libox[num].classList.add('active');
+} */
+
+/* 
+function vlist(e,n){
+    const libox = document.getElementsByClassName('libox');
+    for(let i = 0 ; i<libox.length ; i++)
+    {
+        libox[i].classList.remove('active');
+    }
+    libox[e].classList.add('active');
+    
+    const el = document.querySelectorAll(".product-images");
+    el.forEach((items)=>{
+       items.classList.remove('col-lg-3', 'col-lg-4', 'col-lg-12');
+       items.classList.add(n);     
+    });
+ } */
+
---
+++ js/shoplist.js
@@ -0,0 +1,91 @@
+fetch('data/shoplist.json')
+.then((res)=> res.json())
+.then((res)=>{
+    let rs = res.shoplist;
+   let li = '';
+       for(let i = 0 ; i < rs.length ; i++)
+       {
+           li += '<div class="col-lg-3 my-5">'+
+           '<a href="./main.html">'+
+           '<div class="card">'+
+               '<img src="'+rs[i].img+'" class="img-fluid" alt="033">'+
+               '<div class="card-body">'+
+                   '<ul class="pd-list">'+
+                       '<li>';
+                           
+                           for(let j = 0 ; j < rs[i].color.length ; j++)
+                           {
+                               li+=`<span class=${rs[i].color[j]}></span>`;
+                           }
+                       li+='</li>'+
+                       '<li class="pd-title">'+
+                           '<h1>'+rs[i].title+'</h1>'+
+                       '</li>'+
+                       '<li>'+
+                           '<del>'+rs[i].delprice+'</del><br>'+
+                           '<strong>'+rs[i].saleprice+'</strong>'+
+                       '</li>'+
+                       '<li>'+
+                           '<p>'+rs[i].content+'</p>'+
+                       '</li>'+
+                       '<li class="last">'+
+                           '<span>'+rs[i].news[0]+'</span>'+
+                           '<span>'+rs[i].news[1]+'</span>'+
+                       '</li>'+
+                   '</ul>'+
+               '</div>'+
+            '</div>'+
+            '</a>'+
+       '</div>'
+       }
+       document.getElementById('row').innerHTML = li;
+   })
+   .catch((err)=>console.log(error));
+
+   /* fetch('./data/shoplist.json')
+.then((res)=>res.json())
+.then((res)=> {
+    const rs = res.shoplist; 
+    let div = "";
+
+    for(let i = 0; i < rs.length; i++){
+      div += `<div class="col-lg-3 my-5">
+        <div class="card">
+            <a href="#">
+              <img src="${rs[i].img}" class="img-fluid" alt="${rs[i].img}" />
+            </a>
+            <div class="card-body">
+               <ul class="pd-list">
+                  <li>`;
+
+      var color = '';   
+      for(let c=0; c < rs[i].color.length; c++){
+        color +=  `<span class="${rs[i].color[c]}"></span>`;
+      }             
+      div += color;            
+      div += `</li>
+                  <li class="pd-title">
+                      <h1>${rs[i].title}</h1>
+                  </li>
+                  <li>
+                      <del>${rs[i].delprice}</del>
+                      <br>
+                      <strong>${rs[i].saleprice}</strong>
+                  </li>
+                  <li>
+                      <p>${rs[i].content}</p>
+                  </li>
+                  <li class="last">
+                     <span>베스트셀러</span>
+                     <span>핫딜할인5%</span>
+                  </li>
+               </ul> 
+            </div>
+        </div>
+     </div>`;
+    }
+
+    document.getElementById("row").innerHTML = div;
+
+})
+.catch((err)=> console.log(err)); */
